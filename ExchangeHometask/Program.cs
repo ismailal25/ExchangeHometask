8412a9f7776ff72026d6814e1dd668c404caf2e2
ismailal25 <ismailahiskali99@gmail.com>
2024-05-20 17:22:59 +0400

    Add project files.

---
+++ ExchangeHometask/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ExchangeHometask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] exchange = { 18.94, 0.59, 0.54 };
+            string[] exchanegTypes = { "TL", "USD", "EUR" };
+
+            while (true)
+            {
+                Console.WriteLine("Meblegi Daxil edin");
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
